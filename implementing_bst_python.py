85c9989dd162b5f8b6f67a5911ceaa8fa981a5d1
Divyansh Singh <divyanshsinghrm@gmail.com>
2021-06-04 16:31:30 +0530

    Update implementing_bst_python.py

--- implementing_bst_python.py
+++ implementing_bst_python.py
@@ -1,35 +1,32 @@
-class node:
-    def __init__(self,data=0,left=None,right=None):
-        self.data=data
+class Node:
+    def __init__(self,key=0,left=None,right=None):
+        self.val=key
         self.left=left
         self.right=right
 
-class bst:
-    def __init__(self,root=None):
-        self.root=root
-    def add(self,val):
-        n = node(val)
-        if self.root is None:
-            self.root = n
-
-        t = self.root
-        while not ((t.left is None) and (t.right is None)):
-            if t.data > val:
-                t = t.left
-            elif (t.data<=val):
-                t = t.right
-            if t is None:
-                t=n
-
-        if t.data > val:
-            t.left = n
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if root.val == key:
+            return root
+        elif root.val < key:
+            root.right = insert(root.right, key)
         else:
-            t.right = n
+            root.left = insert(root.left, key)
+    return root
 
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.val)
+        inorder(root.right)
 
-x=node(5)
-tree=bst(x)
-tree.add(3)
-print(tree.root.left.data)
-tree.add(7)
-print(tree.root.right)
+r = Node(49)
+r = insert(r, 30)
+r = insert(r, 20)
+r = insert(r, 40)
+r = insert(r, 70)
+r = insert(r, 60)
+r = insert(r, 80)
+inorder(r)
