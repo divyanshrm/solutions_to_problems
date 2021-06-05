092265791989f572d4bea20c0eeb2b6a1721b489
Divyansh Singh <divyanshsinghrm@gmail.com>
2021-06-05 19:14:57 +0530

    Update implementing_bst_python.py

--- implementing_bst_python.py
+++ implementing_bst_python.py
@@ -1,32 +1,79 @@
-class Node:
-    def __init__(self,key=0,left=None,right=None):
-        self.val=key
+
+class node:
+    def __init__(self,data=0,left=None,right=None):
+        self.data=data
         self.left=left
         self.right=right
 
-def insert(root, key):
-    if root is None:
-        return Node(key)
-    else:
-        if root.val == key:
-            return root
-        elif root.val < key:
-            root.right = insert(root.right, key)
+    def insert(self,data):
+        if data>=self.data:
+            if self.right is None:
+                self.right=node(data)
+            else:
+                self.right.insert(data)
         else:
-            root.left = insert(root.left, key)
-    return root
+            if self.left is None:
+                self.left=node(data)
+            else:
+                self.left.insert(data)
+    def print_tree(self,order,c=0):
+        if c==0:
+            if order.lower() in set(['pre','in','post']):
+                print(order+'order traversal: ')
+            c+=1
+        if order.lower()=='in':
+            if self:
+                if self.left:
+                    self.left.print_tree(order,c)
+                print(self.data)
+                if self.right:
+                    self.right.print_tree(order,c)
+            else:
+                return
+        elif order.lower()=='pre':
 
-def inorder(root):
-    if root:
-        inorder(root.left)
-        print(root.val)
-        inorder(root.right)
+            if self:
+                print(self.data)
+                if self.left:
+                    self.left.print_tree(order,c)
+                if self.right:
+                    self.right.print_tree(order,c)
+            else:
+                return
+        elif order.lower()=='post':
 
-r = Node(49)
-r = insert(r, 30)
-r = insert(r, 20)
-r = insert(r, 40)
-r = insert(r, 70)
-r = insert(r, 60)
-r = insert(r, 80)
-inorder(r)
+            if self:
+                if self.left:
+                    self.left.print_tree(order,c)
+
+                if self.right:
+                    self.right.print_tree(order,c)
+                print(self.data)
+            else:
+                return
+        else:
+            print('Wrong Input ')
+            return
+
+    def findval(self, lkpval):
+        if lkpval < self.data:
+            if self.left is None:
+                print( str(lkpval) + " Not Found")
+                return
+            return self.left.findval(lkpval)
+        elif lkpval > self.data:
+            if self.right is None:
+                print(str(lkpval) + " Not Found")
+                return
+            return self.right.findval(lkpval)
+        else:
+            print(str(self.data) + ' is found')
+tree=node(5)
+tree.insert(3)
+tree.insert(7)
+tree.insert(1)
+tree.insert(4)
+tree.print_tree('pre')
+tree.print_tree('post')
+tree.print_tree('in')
+tree.print_tree('invalid')
